583665008b4f3a103575ee7cc631ce34e9c3993a
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-07-28 15:30:34 -0400

    finished intitilzing fullcalender

--- app/teams/components/Calender.tsx
+++ app/teams/components/Calender.tsx
@@ -1,29 +1,53 @@
 "use client"
 
+import React, { useState } from 'react';
+import {BiMessageSquareAdd} from 'react-icons/bi'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction"; 
 import "./calender.css"
+import Modal from '@/app/components/Modal';
 
 const events = [
   { title: 'Event 1', date: '2023-08-01' },
-
 ]
 
 function Calendar() {
   const handleDateClick = (arg: any) => {
     alert('date clicked ' + arg.dateStr)
-
   }
 
   return (
-    <FullCalendar
+    <>
+    <Modal
+    isOpen={true}
+    >
+
+    </Modal>
+        <FullCalendar
       plugins={[dayGridPlugin, interactionPlugin]}
       initialView="dayGridMonth"
       events={events}
+      dayCellContent={({ date, view }) => <DayCell date={date} />}
       dateClick={handleDateClick}
+      dayCellClassNames="cursor-pointer"
     />
+    </>
+
   )
 }
 
-export default Calendar
+export default Calendar;
+
+const DayCell = ({ date }: any) => {
+  return (
+    <div 
+      className='relative flex flex-col items-start justify-start h-full' 
+    >
+    
+      <div className='z-0'>
+        {date.getDate()}
+      </div>
+    </div>
+  );
+};
