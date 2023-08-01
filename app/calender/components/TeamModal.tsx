0e0239d9a0640bcd10dfa11796f16eaa2f53290e
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-08-01 01:29:00 -0400

    added team modal

---
+++ app/calender/components/TeamModal.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+
+interface TeamModalProps {
+  
+}
+const TeamModal: FC<TeamModalProps> = ({
+  
+}) => {
+  return (
+    <div>
+      TeamModal
+    </div>
+  )
+}
+
+export default TeamModal
