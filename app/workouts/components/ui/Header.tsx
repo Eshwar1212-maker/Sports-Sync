df3e569d720e383d80d9ef9248b048c46f362a96
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-08-07 20:22:32 -0400

    progressed on workout trackers ui

--- app/workouts/components/ui/Header.tsx
+++ app/workouts/components/ui/Header.tsx
@@ -1,24 +1,27 @@
 "use client"
 
 import Button from '@/app/components/Button'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { HiOutlineCalendarDays } from 'react-icons/hi2'
 import { IoMdAdd } from 'react-icons/io'
 import { LiaCalendarDaySolid } from 'react-icons/lia'
+import AddWorkoutModal from './AddWorkoutModal'
 interface HeaderProps {
   
 }
 const Header: FC<HeaderProps> = ({
   
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className='flex py-8 justify-between px-[500px]'>
+    <div className='flex py-8 justify-between 2xl:px-[500px]'>
+      <AddWorkoutModal isOpen={isOpen} onClose={() => setIsOpen(false)}/>
       <div>
         <button>
         <LiaCalendarDaySolid size={50}/>
         </button>
         </div>
-      <div>
+      <div onClick={() => setIsOpen(true)}>
          <Button>
             Add Workout
          </Button>
