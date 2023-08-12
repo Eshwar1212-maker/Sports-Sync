47d595173a9766e2a7088728ea271a7f692558ae
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-08-12 00:36:01 -0400

    fixed css height issue

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,7 +2,6 @@
 
 import { User } from "@prisma/client";
 
-import useActiveList from "../hooks/useActiveList";
 import Image from "next/image";
 
 interface AvatarProps {
@@ -10,8 +9,7 @@ interface AvatarProps {
 };
 
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
-  const { members } = useActiveList();
-  const isActive = members.indexOf(user?.email!) !== -1;
+
 
   return (
     <div className="relative">
@@ -31,24 +29,6 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
           alt="Avatar"
         />
       </div>
-      {isActive ? (
-        <span 
-          className="
-            absolute 
-            block 
-            rounded-full 
-            bg-green-500 
-            ring-2 
-            ring-white 
-            top-0 
-            right-0
-            h-2 
-            w-2 
-            md:h-3 
-            md:w-3
-          " 
-        />
-      ) : null}
     </div>
   );
 }
