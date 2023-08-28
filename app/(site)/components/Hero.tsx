7ba0b9471cda98fc7baa4e53920965ecc5e1f550
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-08-28 15:40:59 -0400

    changes

--- app/(site)/components/Hero.tsx
+++ app/(site)/components/Hero.tsx
@@ -32,10 +32,10 @@ const Hero = () => {
   const router = useRouter();
   return (
     <div className="bg-white text-black">
-      <div className="flex flex-col justify-center pb-11 md:py-20 mb-6 md:h-[75vh] lg:flex-row lg:h-[70vh] lg:px-11 xl:px-[100px] 2xl:px-[290px]">
-        <div className="sm:w-full lg:w-1/2 p-4 flex flex-col lg:items-start lg:space-y-6">
+      <div className="flex flex-col justify-between pb-11 py-20 sm:my-12 mb-6 md:h-[75vh] lg:flex-row lg:h-[70vh] lg:px-11 xl:px-[100px] 2xl:px-[90px]">
+        <div className="sm:w-full lg:w-1/2 p-4 flex flex-col lg:items-start lg:space-y- lg:pl-40">
           <div className="space-y-5 text-center lg:text-left">
-            <h1 style={inter.style} className="text-4xl 2xl:text-6xl font-bold">
+            <h1 style={inter.style} className="text-4xl 2xl:text-6xl font-bold my-4">
               The world's best platform for athletes
             </h1>
             <p
