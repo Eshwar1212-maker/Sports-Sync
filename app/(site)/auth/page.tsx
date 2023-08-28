f13821e420221a2deeed8a0d43e0ffb44d23bd1e
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-08-28 15:29:00 -0400

    changes

--- app/(site)/auth/page.tsx
+++ app/(site)/auth/page.tsx
@@ -7,17 +7,16 @@ const page: FC<pageProps> = ({}) => {
   return (
     <>
         <div 
-    style={{ backgroundImage: 'url(/auth.jpg)', backgroundPosition: "center 29%" }}
+    style={{ backgroundImage: 'url(/auth.jpg)', backgroundPosition: "center 28%" }}
     className="hidden sm:flex h-screen min-h-full flex-col py-[131px] sm:px-6 lg:px-8 bg-gradient-to-t bg-cover bg-no-repeat">
       {/*Auth Form*/}
       <AuthForm />
-      <p className="text-gray-100 m-auto font-semibold max-w-[430px] pb-[110px] pl-5">Passion brings communities together, communities foster growth, growth gets us to the end result.</p>
+      <p className="text-gray-100 m-auto font-semibold max-w-[455px] pb-[110px] pl-5">Passion brings communities together, communities foster growth, growth gets us to the end result.</p>
     </div>
     <div 
     className="flex h-screen min-h-full flex-col py-[131px] sm:px-6 lg:px-8 bg-gradient-to-t bg-cover bg-no-repeat sm:hidden">
       {/*Auth Form*/}
       <AuthForm />
-      <p className="text-gray-100 m-auto font-semibold max-w-[430px] pb-[110px] pl-5">Passion brings communities together, communities foster growth, growth gets us to the end result.</p>
     </div>
     </>
 
