d47076e916551b7f8c5cce3a7c6fbd88946fd71a
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-08-28 16:42:41 -0400

    changes

--- app/(site)/auth/page.tsx
+++ app/(site)/auth/page.tsx
@@ -7,11 +7,11 @@ const page: FC<pageProps> = ({}) => {
   return (
     <>
         <div 
-    style={{ backgroundImage: 'url(/auth.jpg)', backgroundPosition: "center 28%" }}
-    className="hidden sm:flex h-screen min-h-full flex-col py-[131px] sm:px-6 lg:px-8 bg-gradient-to-t bg-cover bg-no-repeat">
+    style={{ backgroundImage: 'url(/auth.jpg)', backgroundPosition: "center 21%" }}
+    className="hidden sm:flex h-screen min-h-full flex-col py-[218px] sm:px-6 lg:px-8 bg-gradient-to-t bg-cover bg-no-repeat">
       {/*Auth Form*/}
       <AuthForm />
-      <p className="text-gray-100 m-auto font-semibold max-w-[455px] pb-[110px] pl-5">Passion brings communities together, communities foster growth, growth gets us to the end result.</p>
+      <p className="text-gray-100 m-auto font-semibold max-w-[455px] pb-[110px] pl-5 text-lg">Passion brings communities together, communities foster growth, growth gets us to the end result.</p>
     </div>
     <div 
     className="flex h-screen min-h-full flex-col py-[131px] sm:px-6 lg:px-8 bg-gradient-to-t bg-cover bg-no-repeat sm:hidden">
--- app/(site)/components/Hero.tsx
+++ app/(site)/components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
       <div className="flex flex-col justify-between pb-11 py-20 sm:my-12 mb-6 md:h-[75vh] lg:flex-row lg:h-[70vh] lg:px-11 xl:px-[100px] 2xl:px-[90px]">
         <div className="sm:w-full lg:w-1/2 p-4 flex flex-col lg:items-start lg:space-y-6 2xl:pl-40">
           <div className="space-y-5 text-center lg:text-left">
-            <h1 style={inter.style} className="text-4xl 2xl:text-6xl font-bold my-4">
+            <h1 style={inter.style} className="text-4xl lg:text-5xl 2xl:text-6xl font-bold my-4">
               The world's best platform for athletes
             </h1>
             <p
