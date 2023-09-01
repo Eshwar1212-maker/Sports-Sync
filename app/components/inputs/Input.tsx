4f5e189b77d4d84b7585126e96d6fe32292adaed
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-09-01 15:17:04 -0400

    changes

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -22,7 +22,7 @@ const Input: FC<InputProps> = ({
   const currentTheme = theme === "system" ? systemTheme : theme;
   return (
     <div className=''>
-      <label htmlFor={id} className='block text-sm font-md leading-6'>{label}</label>
+      <label htmlFor={id} className='block text-sm font-md leading-6 text-black'>{label}</label>
       <div className='mt-2'>
             <input
             placeholder={placeholder}
@@ -32,7 +32,7 @@ const Input: FC<InputProps> = ({
             autoComplete={id} 
             {...register(id, {required})}
             className={clsx(`
-                form-input block w-full ronded-md border-0 py-1.5 shadow-sm ring-1 px-2
+                form-input block w-full ronded-md border-0 py-1.5 shadow-sm ring-1 px-2 bg-white text-black
                 ring-inset ring-gray-300 placeholder:text-gray-400focus focus:ring-2 focus:ring-inset
                 focus:ring-sky-600 sm:text-sm sm:leading-6
             `, errors[id] && "focus: ring-rose-500", disabled && "opacity-50 cursor-default",
