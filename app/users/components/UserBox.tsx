967154304cf09cf0b4106bbdd688c9e0826ef46a
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-09-08 04:08:51 -0400

    added user profile modal for other users

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,20 +1,33 @@
+"use client"
 import Avatar from "@/app/components/Avatar";
 import LoadingModal from "@/app/components/LoadingModal";
 import { User } from "@prisma/client";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import clsx from "clsx";
 import { useTheme } from "next-themes";
 import { useRouter } from "next/navigation";
-import { FC } from "react";
+import { FC, useState } from "react";
+import { AiOutlineMessage, AiOutlineUserAdd } from "react-icons/ai";
+import { BiMessageAdd } from "react-icons/bi";
+import { FiMessageCircle } from "react-icons/fi";
+import ProfileModal from "./ProfileModal";
 
 interface UserBoxProps {
   data: User;
-  input?: string
+  input?: string;
 }
 
 const UserBox: FC<UserBoxProps> = ({ data, input }) => {
   const router = useRouter();
+  const [isProfileOpen, setIsProfileOpen] = useState(false)
+  const [clickedUser, setClickedUser] = useState<any>()
   const {
     mutate: createConversation,
     isLoading,
@@ -35,7 +48,6 @@ const UserBox: FC<UserBoxProps> = ({ data, input }) => {
   );
   const { systemTheme, theme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
- 
 
   return (
     <>
@@ -44,16 +56,58 @@ const UserBox: FC<UserBoxProps> = ({ data, input }) => {
       ) : (
         <div
           className={clsx(
-            'w-full relative flex items-center space-x-3 p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer',
-            currentTheme === "dark" ? "hover:bg-neutral-800" : "hover:bg-neutral-100" 
+            "w-full relative flex items-center space-x-3 p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer",
+            currentTheme === "dark"
+              ? "hover:bg-neutral-800"
+              : "hover:bg-neutral-100"
           )}
-          onClick={() => createConversation()}
         >
+          <ProfileModal
+            isOpen={isProfileOpen}
+            onClose={() => setIsProfileOpen(false)}
+            user={clickedUser}
+           />
           <Avatar user={data} />
           <div className="min-w-0 flex-1">
             <div className="focus:outline-none">
               <div className="flex justify-between items-center mb-1">
-                <p className="text-sm font-md">{data.name}</p>
+                <div>
+                  <p className="text-sm font-md">{(data.name?.split(" ")[0] +  " " + data.name?.split(" ")[1]).substring(0, 16)}</p>
+                </div>
+                <div className="flex gap-2 pl-4">
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <AiOutlineUserAdd
+                         onClick={() => {
+                          console.log(data);
+                          setClickedUser(data)
+                          setIsProfileOpen(true)
+                        }}
+                         size={24} 
+                         />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className="text-[13px] pb-3">View Profile</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        
+                  <FiMessageCircle
+                    onClick={() => createConversation()}
+                    size={24}
+                  />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className="text-[13px] pb-3">Message {data.name}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+
+                </div>
               </div>
             </div>
           </div>
