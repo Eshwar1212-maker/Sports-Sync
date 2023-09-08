3906dcdfef71ce946a8b08f579172d4401f22eb9
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-09-07 23:52:49 -0400

    added hover for secondary button variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         outline:
           " bg-white hover:bg-slate-100 hover:text-slate-900 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         secondary:
-          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+          "bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
         ghost: "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
