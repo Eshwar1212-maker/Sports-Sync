7db97c76b7789a60cb3b0764dae0a5c45f14bf85
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-09-13 17:27:04 -0400

    added landing page routes to showcase new featuers added

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/landing/Footer";
 
 export default function LandingLayout({
   children, // will be a page or nested layout
@@ -9,6 +10,7 @@ export default function LandingLayout({
     <section>
       <Navbar />
       {children}
+      <Footer />
     </section>
   );
 }
--- app/(site)/workspaces/components/WorkSpaceThird.tsx
+++ app/(site)/workspaces/components/WorkSpaceThird.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { FC } from "react";
 import image from "../../../assets/Synced/workspacesidebar.png"
+import Footer from "../../components/landing/Footer";
 
 interface WorkSpaceFirstProps {}
 const WorkSpaceThird: FC<WorkSpaceFirstProps> = ({}) => {
   return (
-    <div className=" bg-blue-100 h-[100vh] lg:h-[760px] lg:py-11">
+    <div className=" bg-blue-100 h-[100vh] lg:h-[760px] lg:py-11 hidden">
       <div className="flex flex-col lg:flex-row lg:justify-between sm:max-w-[900px] lg:max-w-[1000px] xl:max-w-[1200px] 2xl:max-w-[1400px] mx-auto h-[400px] lg:py-11">
         <div className="mx-auto my-auto max-w-[350px] sm:max-w-[500px] pt-[70px] pb-10 lg:py-[120px] text-2xl lg:text-4xl font-thin">
             Fully customizable workspaces for individuals and teams. <br />
