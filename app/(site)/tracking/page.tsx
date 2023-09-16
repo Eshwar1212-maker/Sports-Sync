4bb10769778110a4b971b91ae34bb81265a58ee6
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-09-16 18:02:43 -0400

    added and fixed a lot of ui on the landing page

--- app/(site)/tracking/page.tsx
+++ app/(site)/tracking/page.tsx
@@ -2,7 +2,6 @@
 
 import TrackerHero from './components/TrackerHero'
 import TrackerFirst from './components/TrackerFirst'
-import TrackerSecond from './components/TrackerSecond'
 
 
 const page = () => {
