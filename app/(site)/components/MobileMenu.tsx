7f1b464a96b27e5a1bf6c92005fc11daf4859f91
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-09-15 21:39:55 -0400

    Currently working on mobile menuy

--- app/(site)/components/MobileMenu.tsx
+++ app/(site)/components/MobileMenu.tsx
@@ -19,12 +19,14 @@ const itemVariants: any = {
   closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
 };
 const MobileMenu: FC<MobileMenuProps> = ({ isOpen, onClose }) => {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <div className="md:hidden">
+    <div className="md:hidden relative">
       <button
         onClick={onClose}
-        className={"fixed right-4 transition ease-in-out duration-500 cursor-pointer rounded-lg max-h-[49px]"}
+        className={
+          "fixed right-4 transition ease-in-out duration-500 cursor-pointer rounded-lg max-h-[49px]"
+        }
       >
         {!isOpen ? <RiMenu3Line size={24} /> : <AiOutlineClose size={24} />}
       </button>
@@ -63,33 +65,61 @@ const MobileMenu: FC<MobileMenuProps> = ({ isOpen, onClose }) => {
             }}
             className="h-screen mx-auto text-center space-y-6 mt-20 flex flex-col mr-[160px] sm:mr-[180px] font-light"
           >
-            <motion.li onClick={() => {router.push("/auth")}} 
-            variants={itemVariants} 
-            className="py-3 cursor-pointer text-[18px] border-b-[1px] border-slate-400 transition duration-300 hover:bg-slate-200">
+            <motion.li
+              onClick={() => {
+                router.push("/auth");
+              }}
+              variants={itemVariants}
+              className="py-3 cursor-pointer text-[18px] border-b-[1px] border-slate-400 transition duration-300 sm:mr-0 hover:bg-slate-200"
+            >
               Get Started
             </motion.li>
-            <motion.li onClick={() => {router.push("/workspaces")}} 
-            variants={itemVariants} 
-            className="py-3 cursor-pointer text-[18px] border-b-[1px] border-slate-400 transition duration-300 hover:bg-slate-200">
+            <motion.li
+              onClick={() => {
+                router.push("/workspaces");
+              }}
+              variants={itemVariants}
+              className="py-3 cursor-pointer text-[18px] border-b-[1px] border-slate-400 transition duration-300 sm:mr-0 hover:bg-slate-200"
+            >
               WorkSpaces
             </motion.li>
-            <motion.li onClick={() => {router.push("/tracking")}} 
-            variants={itemVariants} 
-            className="py-3 cursor-pointer text-[18px] border-b-[1px] border-slate-400 transition duration-300 hover:bg-slate-200">
+            <motion.li
+              onClick={() => {
+                router.push("/tracking");
+              }}
+              variants={itemVariants}
+              className="py-3 cursor-pointer text-[18px] border-b-[1px] border-slate-400 transition duration-300 sm:mr-0 hover:bg-slate-200"
+            >
               Tracking
             </motion.li>
-            <motion.li onClick={() => {router.push("/techstack")}} 
-            variants={itemVariants} 
-            className="py-3 cursor-pointer text-[18px] border-b-[1px] border-slate-400 transition duration-300 hover:bg-slate-200">
+            <motion.li
+              onClick={() => {
+                router.push("/techstack");
+              }}
+              variants={itemVariants}
+              className="py-3 cursor-pointer text-[18px] border-b-[1px] border-slate-400 transition duration-300 sm:mr-0 hover:bg-slate-200"
+            >
               Tech Stack
             </motion.li>
-            <div onClick={() => {router.push("/auth")}} 
-            className="flex gap-2 flex-col mx-auto justify-center">
-             <Button variant={"four"} className="w-[240px] mx-auto text-center items-center">Get Started</Button>
-             <Button className="w-[240px] mx-auto text-center items-center rounded-lg">Log In</Button>
-            </div>
+            <motion.li
+              onClick={() => {
+                router.push("/techstack");
+              }}
+              variants={itemVariants}
+              className="py-3 cursor-pointer text-[18px] transition duration-300 hover:bg-slate-200 flex flex-col gap-2"
+            >
+              <Button
+                variant={"four"}
+                className="w-[240px] mx-auto text-center items-center rounded-lg"
+              >
+                Get Started
+              </Button>
+              <Button className="w-[240px] mx-auto text-center items-center rounded-lg">
+                Log In
+              </Button>
+            </motion.li>
+ 
           </motion.ul>
-      
         </motion.div>
       )}
     </div>
