b5bb45539b74a62cf67becb9ccebae55c0b3a4af
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-09-23 11:06:11 -0400

    fixed time bug when adding a event from the workout log to the calender

--- app/calender/userCalender/layout.tsx
+++ app/calender/userCalender/layout.tsx
@@ -15,20 +15,7 @@ export default async function CalenderLayout({
 
   return (
     <Sidebar>
-            <Link
-          href="/calender" 
-          className="
-            lg:hidden 
-            block 
-            text-sky-500 
-            hover:text-sky-600 
-            transition 
-            cursor-pointer
-          "
-        >
-          <HiChevronLeft size={32} />
-        </Link>
-      <div className="py-8 px-10 xl:px-0 xl:pr-20 text-[12px]">
+      <div className="py-8 px-10 lg:px-0 lg:mr-20 text-[12px]">
         <Calender userEvents={userEvents} />
         {children}
       </div>
