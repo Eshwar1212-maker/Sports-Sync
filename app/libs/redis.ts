d0ac7361a482e2492daef03dc3424ead6dacee1c
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-09-24 17:52:30 -0400

    added redis for caching

--- app/libs/redis.ts
+++ app/libs/redis.ts
@@ -5,7 +5,7 @@ const getRedisUrl = () => {
   if(process.env.REDIS_URL){
     return process.env.REDIS_URL
   }
-  throw new Error("NO process.env.UPSTASH_REDIS_REST_UR")
+  throw new Error("NO REDIS URL")
 }
 
 export const redis = new Redis(getRedisUrl())
