0af6fa3c43824c29483502b7fca3b3de2d3b98d5
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-10-01 16:25:10 -0400

    added redis cache for users route

--- app/actions/getUsers.ts
+++ app/actions/getUsers.ts
@@ -1,12 +1,14 @@
 import getSession from "./getSession"
 import prisma from "@/app/libs/prismadb";
 import { redis } from "../libs/redis";
+import getCurrentUser from "./getCurrentUser";
 
 const getUsers = async () => {
+    const currentUser = await getCurrentUser()
     const session = await getSession();
     if (!session?.user?.email) return [];
     try {
-        const cache = await redis.get(`users`);
+        const cache = await redis.get(`${currentUser}users`);
         if (cache) {
             //console.log("HITTING REDIS CACHE ", start - Date.now()); // Uncomment if you need this log
             return JSON.parse(cache);
@@ -21,8 +23,8 @@ const getUsers = async () => {
                     }
                 }
             });
-            await redis.set(`users`, JSON.stringify(users));
-            await redis.expire(`users`, 20000);
+            await redis.set(`${currentUser}users`, JSON.stringify(users));
+            await redis.expire(`${currentUser}users`, 20000);
             return users;
         }
     } catch (error) {
