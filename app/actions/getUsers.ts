65fd6fb3b00dca8ee0e2ef5800b2bd82a0f57906
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-10-01 16:30:00 -0400

    added redis cache for users route

--- app/actions/getUsers.ts
+++ app/actions/getUsers.ts
@@ -8,7 +8,7 @@ const getUsers = async () => {
     const session = await getSession();
     if (!session?.user?.email) return [];
     try {
-        const cache = await redis.get(`${currentUser}users`);
+        const cache = await redis.get(`${currentUser?.name}users`);
         if (cache) {
             //console.log("HITTING REDIS CACHE ", start - Date.now()); // Uncomment if you need this log
             return JSON.parse(cache);
@@ -23,8 +23,8 @@ const getUsers = async () => {
                     }
                 }
             });
-            await redis.set(`${currentUser}users`, JSON.stringify(users));
-            await redis.expire(`${currentUser}users`, 20000);
+            await redis.set(`${currentUser?.name}users`, JSON.stringify(users));
+            await redis.expire(`${currentUser?.name}users`, 20000);
             return users;
         }
     } catch (error) {
