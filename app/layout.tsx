5a004b00e33a7edd4a91af5878c019661dc584f6
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-10-03 18:11:20 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <AuthContext>
           <ThemeProviders>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  favicon: {
+    src: "/favicon.ico"
+  }
+  ,
   experimental: {
   },
   images: {
