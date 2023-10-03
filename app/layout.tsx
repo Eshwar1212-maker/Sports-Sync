178010307c557c4c1f51fa7443aaac959413deb9
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-10-03 11:23:28 -0400

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,40 @@
-
-import Providers from './components/Providers'
-import AuthContext from './context/AuthContext'
-import ToasterContext from './context/ToasterContext'
-import './globals.css'
-import ThemeProviders from './context/ThemeProviders'
- 
+import Providers from "./components/Providers";
+import AuthContext from "./context/AuthContext";
+import ToasterContext from "./context/ToasterContext";
+import "./globals.css";
+import ThemeProviders from "./context/ThemeProviders";
 
 export const metadata = {
-  title: 'Synced',
-  description: 'Workout/productvity app for athletes and teams',
+  title: "Synced",
+  description: "Workout/productvity app for athletes and teams",
   icons: {
     shortcut: "../app/icon.jpeg",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="../app/icon.jpeg"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body>
         <AuthContext>
-        <ThemeProviders>
-          <Providers>
-            {children}
-          </Providers>
+          <ThemeProviders>
+            <Providers>{children}</Providers>
           </ThemeProviders>
           <ToasterContext />
         </AuthContext>
       </body>
     </html>
-  )
+  );
 }
