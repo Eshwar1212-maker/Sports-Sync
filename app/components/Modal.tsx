df2531b8ff71aab340bda70db653d26c37558023
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-10-08 22:42:57 -0400

    changes

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -58,17 +58,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, isFullWidth, i
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel 
-                className={clsx(`
-                relative 
-                transform
-                overflow-hidden 
-                rounded-lg 
-                px-4 
-                pb-4
-                pt-5 
-                text-left 
-                shadow-xl 
-                transition-all
+                className={clsx(`relative transform overflow-hidden px-4 pb-4 pt-5 text-left shadow-xl transition-all
               `, currentTheme == "dark" ? "bg-[#1c1c1c]" : "bg-white",
               (isFullWidth && isImage && !isMessage) ? "w-[95%] h-[694px] md:w-[900px] md:h-[712px]" : "w-full sm:my-8 sm:w-full sm:max-w-lg sm:p-6",
 
