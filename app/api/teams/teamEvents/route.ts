0f6d1ea78de3b55213139a2b945f42c63c99b9b7
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-10-13 13:29:44 -0400

    work

--- app/api/teams/teamEvents/route.ts
+++ app/api/teams/teamEvents/route.ts
@@ -2,16 +2,18 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 import { redis } from "@/app/libs/redis";
+import getUserById from "@/app/actions/getUserById";
 
 export async function POST(request: Request) {
   try {
     const currentUser = await getCurrentUser();
     const body = await request.json();
-    const { notes, title, date, teamId, poster} = body;    
+    const { notes, title, date, teamId, poster } = body;
 
     if (!currentUser?.id || !currentUser?.email) {
       return new NextResponse("Unauthorized", { status: 400 });
     }
+    //ADDING THE EVENT
     const newEvent = await prisma.event.create({
       data: {
         notes,
@@ -20,22 +22,49 @@ export async function POST(request: Request) {
         poster,
         user: {
           connect: {
-            id: currentUser.id
+            id: currentUser.id,
           },
         },
         Team: {
-            connect: {
-                id: teamId
-            }
-        }
+          connect: {
+            id: teamId,
+          },
+        },
       },
     });
+
+    //SENDING NOTIFICATIONS TO ALL TEAM MEMBERS FOR THE NEW EVENT ADDED
+    const team = await prisma.team.findUnique({
+      where: {
+        id: newEvent?.teamId!,
+      },
+      select: {
+        userIds: true,
+      },
+    });
+    console.log("TEAMS USER ID's: ", team?.userIds);
+    const teamMembers = team?.userIds.map(async (member: string) => {
+      return await prisma.user.findUnique({
+        where: {
+          id: member,
+        },
+        select: {
+          email: true,
+        },
+      });
+    });
+
+    const teamMemberEmails = await (await Promise.all(teamMembers!)).map((m) => m?.email)
+    console.log("TEAM MEMBERS: ", teamMemberEmails);
+
+ 
 
 
     await redis.del(`${teamId}team`);
 
     return NextResponse.json(newEvent);
   } catch (error) {
+    console.log(error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
