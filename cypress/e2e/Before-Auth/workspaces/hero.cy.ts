42726a4112a0ef0aed385560f103686d01c40354
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-12-02 13:02:22 -0500

    Testing Cypress

--- cypress/e2e/Before-Auth/workspaces/hero.cy.ts
+++ cypress/e2e/Before-Auth/workspaces/hero.cy.ts
@@ -4,4 +4,4 @@ describe('hero', () => {
       cy.get('[data-test="workspace-title"]').contains("Create your own team calendars")
       cy.get('[data-test="workspace-hero-description"]').contains("Synced is the easiest way for individual athletes or teams to plan, manage, and visualize their events in a shared team calendar.")
     })
-  })  
+  })   
