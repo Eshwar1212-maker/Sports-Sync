35322b2b7848d3a6dce81f24b0723bc0d44ff3f8
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-12-02 13:05:41 -0500

    Testing Cypress to work with failes status codes

--- cypress/e2e/Before-Auth/workspaces/hero.cy.ts
+++ cypress/e2e/Before-Auth/workspaces/hero.cy.ts
@@ -1,6 +1,6 @@
 describe('hero', () => {
     it('Navbar contains correct logo', () => {
-      cy.visit('/workspaces')
+      cy.visit('/workspaces', { failOnStatusCode: false });
       cy.get('[data-test="workspace-title"]').contains("Create your own team calendars")
       cy.get('[data-test="workspace-hero-description"]').contains("Synced is the easiest way for individual athletes or teams to plan, manage, and visualize their events in a shared team calendar.")
     })
