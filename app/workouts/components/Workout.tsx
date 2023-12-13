79259f614b8f3bb1bd2fa8f284ee0159e010b330
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-12-13 12:50:26 -0500

    Removed copy workout feature on small screens

--- app/workouts/components/Workout.tsx
+++ app/workouts/components/Workout.tsx
@@ -285,7 +285,7 @@ const Workout: FC<WorkoutProps> = ({ workouts, workoutRecord }) => {
         newDate.toString().split(" ").slice(0, 3).toString() ===
           date?.toString().split(" ").slice(0, 3).toString() ? (
           <div
-            className="flex sm:hidden lg:flex gap-3 mr-14 lg:mr-20 hover:bg-gray-200 dark:hover:bg-gray-900 transition
+            className="hidden lg:flex gap-3 mr-14 lg:mr-20 hover:bg-gray-200 dark:hover:bg-gray-900 transition
            ease-in-out duration-200 w-fit cursor-pointer p-3 rounded-lg mx-auto justify-center items-center text-center"
             onClick={() => setIsCopyOpen(true)}
           >
