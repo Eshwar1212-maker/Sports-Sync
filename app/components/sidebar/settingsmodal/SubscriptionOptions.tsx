f1e841c2c08060f8b9067b2f3d54dc16aa5b9678
Eshwar Tangirala <eshwartangirala11@gmail.com>
2024-08-14 14:22:24 -0400

    finishing pro modal

--- app/components/sidebar/settingsmodal/SubscriptionOptions.tsx
+++ app/components/sidebar/settingsmodal/SubscriptionOptions.tsx
@@ -15,7 +15,7 @@ const SubscriptionOptions: FC<SubscriptionOptionsProps> = ({}) => {
       <div className="border-s border-white border-[1px] rounded-md pt-2 px-3 m-2 h-[115px]">
         <div className="flex justify-between">
           <h3 className="font-bold">Plus</h3>
-          <Button className="bg-blue-500 rounded-md hover:bg-blue-600 text-[12px] px-4">
+          <Button className="bg-blue-500 rounded-md hover:bg-blue-600 text-[12px] px-4 h-8">
             Upgrade
           </Button>
         </div>
@@ -29,7 +29,7 @@ const SubscriptionOptions: FC<SubscriptionOptionsProps> = ({}) => {
       <div className="border-s border-white border-[1px] rounded-md pt-2 px-3 m-2 h-[115px]">
         <div className="flex justify-between">
           <h3 className="font-bold">Enterprise</h3>
-          <Button className="bg-slate-600 text-white rounded-md hover:bg-slate-700 text-[12px] px-4">
+          <Button className="bg-slate-600 text-white rounded-md hover:bg-slate-700 text-[12px] px-4 h-8">
             Upgrade
           </Button>
         </div>
